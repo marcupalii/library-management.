e7a77b97b5259675d19f7d360eb72851a81fc126
Marcu Palii <marcupalii@gmail.com>
2019-03-03 20:13:59 +0200

    - remove entry from next_book if wishlist is empty

--- helpers.py
+++ helpers.py
@@ -39,9 +39,11 @@ def _incremet_rank(rank,curr_user):
 
 
 def _add_entry_into_nextbook(user_id):
-    _next_book = NextBook(id_user=user_id, status="None")
-    db.session.add(_next_book)
-    db.session.commit()
+    _next_book = NextBook.query.filter_by(id_user=user_id)
+    if not _next_book:
+        _next_book = NextBook.query.filter_by(id_user=user_id, status="None")
+        db.session.add(_next_book)
+        db.session.commit()
 
 
 def _add_book_to_wishlist(book,curr_user,period,rank):
