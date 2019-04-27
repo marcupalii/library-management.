00033f0ab39c735d47a8da1ba47dd914b7fae752
marcupalii <marcupalii@gmail.com>
2019-04-27 20:39:27 +0300

    fix sidebar transition

--- app/static/js/layout_navbar.js
+++ app/static/js/layout_navbar.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('[data-toggle="collapse"]').click(function () {
 
         let sidebar = $('.row-offcanvas-left');
-        if (sidebar.attr("class") === "order-1 row-offcanvas-left active") {
+        if (sidebar.hasClass("active")) {
             sidebar.toggleClass('active');
             $('.content').css('background-color', 'rgba(0, 0, 0, 0)');
         }
