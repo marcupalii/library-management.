d44c9bfeb7638865d8b5b9d1e11d7007bcb4c19b
marcupalii <marcupalii@gmail.com>
2019-06-06 01:00:22 +0300

    sidebar added z-index-above,  z-index-below css class

--- app/static/js/layout_sidebar.js
+++ app/static/js/layout_sidebar.js
@@ -1,16 +1,34 @@
 $(document).ready(function () {
     $(window).resize(function () {
-        if ($('#button-sidebar').attr("aria-expanded") === "false" && $(window).width() <= 1050 && $(window).width() >= 1010 ) {
+        if ($('#button-sidebar').attr("aria-expanded") === "false" && $(window).width() <= 1050 && $(window).width() >= 1010) {
+            if ($('.row-offcanvas-left').hasClass("overlay-z-index-above") === false) {
+                $('.row-offcanvas-left').addClass("overlay-z-index-above");
+            }
             $('.content')
                 .addClass("overlay-z-index-under")
                 .delay(1500)
                 .queue(function (next) {
-                    $(this).removeClass("overlay-z-index-under");
+                     $(this).removeClass("overlay-z-index-under");
                     next();
                 });
+
         }
     });
-    $('[data-toggle="offcanvas"]').click(function () {
+    var trans = false;
+    $('[data-toggle="offcanvas"]').click(function (e) {
+        if (trans===false){
+            console.log(trans);
+            trans = true;
+        }else{
+            e.stopPropagation();
+            e.preventDefault();
+            return;
+        }
+
+        if (! $('.row-offcanvas-left').hasClass("overlay-z-index-above")) {
+            $('.row-offcanvas-left').addClass("overlay-z-index-above");
+        }
+
 
         let navar_button = $('#navbar-button');
 
@@ -25,14 +43,18 @@ $(document).ready(function () {
             function change_content_index() {
                 if ($('.row-offcanvas-left').hasClass("active") === false) {
                     $('.content').removeClass("overlay-z-index-under");
+                    $('.row-offcanvas-left').removeClass("overlay-z-index-above");
+                    trans = false;
                 }
             }
 
-            setTimeout(change_content_index, 1500);
+            setTimeout(change_content_index, 300);
 
         } else {
             $('.content').addClass("overlay-z-index-under");
+            $('.row-offcanvas-left').addClass("overlay-z-index-above");
             $('.row-offcanvas-left').addClass('active');
+            trans = false;
 
         }
 
