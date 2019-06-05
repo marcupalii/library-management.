ef24f1d6dfad1f7b65de147c23a9dfa16fb38da2
marcupalii <marcupalii@gmail.com>
2019-06-06 01:32:06 +0300

    sidebar final fix

--- app/static/js/layout_sidebar.js
+++ app/static/js/layout_sidebar.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
                 .addClass("overlay-z-index-under")
                 .delay(1500)
                 .queue(function (next) {
-                     $(this).removeClass("overlay-z-index-under");
+                    $(this).removeClass("overlay-z-index-under");
                     next();
                 });
 
@@ -16,17 +16,17 @@ $(document).ready(function () {
     });
     var trans = false;
     $('[data-toggle="offcanvas"]').click(function (e) {
-        if (trans===false){
+        if (!$('.row-offcanvas-left').hasClass("overlay-z-index-above")) {
+            $('.row-offcanvas-left').addClass("overlay-z-index-above");
+        }
+
+        if (trans === false) {
             console.log(trans);
             trans = true;
-        }else{
+        } else {
             e.stopPropagation();
             e.preventDefault();
             return;
-        }
-
-        if (! $('.row-offcanvas-left').hasClass("overlay-z-index-above")) {
-            $('.row-offcanvas-left').addClass("overlay-z-index-above");
         }
 
 
@@ -41,18 +41,20 @@ $(document).ready(function () {
             $('.row-offcanvas-left').removeClass('active');
 
             function change_content_index() {
-                if ($('.row-offcanvas-left').hasClass("active") === false) {
-                    $('.content').removeClass("overlay-z-index-under");
-                    $('.row-offcanvas-left').removeClass("overlay-z-index-above");
-                    trans = false;
-                }
+
+                $('.content').removeClass("overlay-z-index-under");
+                $('.row-offcanvas-left').removeClass("overlay-z-index-above");
+                trans = false;
+
             }
 
-            setTimeout(change_content_index, 300);
+            setTimeout(change_content_index, 700);
 
         } else {
             $('.content').addClass("overlay-z-index-under");
-            $('.row-offcanvas-left').addClass("overlay-z-index-above");
+            if (!$('.row-offcanvas-left').hasClass("overlay-z-index-above")) {
+                $('.row-offcanvas-left').addClass("overlay-z-index-above");
+            }
             $('.row-offcanvas-left').addClass('active');
             trans = false;
 
